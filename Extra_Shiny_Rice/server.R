d18224a0f3387bb3599a27159a568ee38f2f8375
sastinson <sastinson@ucdavis.edu>
2016-05-06 15:30:55 -0700

    subset data to isolate seed traits melt data

--- Extra_Shiny_Rice/server.R
+++ Extra_Shiny_Rice/server.R
@@ -8,22 +8,37 @@
 #
 
 library(shiny)
+library(reshape2)
+library(ggplot2)
 
 load("data_from_SNP_lab.Rdata")
 data.pheno.mds <- merge(geno.mds, data.pheno, by = "row.names", all = TRUE)
 
-# Define server logic required to draw a histogram
+colnames(data.pheno.mds)
+#subset the data to only include the phenotypic traits of interest, popID and Region
+data.pheno.mds1 <- subset(data.pheno.mds[,c(4,11,27,30,31,32,33)])
+head(data.pheno.mds1)
+
+#melt the data
+data.pheno.mds.melt <- melt(data.pheno.mds1)
+colnames(data.pheno.mds.melt)
+
+# Define server logic required to draw a violinplot
 shinyServer(function(input, output) {
    
-  output$distPlot <- renderPlot({
+  output$violinPlot <- renderPlot({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    # set up the plot
+    pl <- ggplot(data = data.pheno.mds.melt[data.pheno.mds.melt$variable == input$variable,],
+                 #Use aes_string below so that input$trait is interpreted
+                 #correctly.  The other variables need to be quoted
+                 aes_string(x="value",
+                            y=input$trait,
+                            fill="variable"
+                 )
+    )
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+    # draw the violinplot for the specified trait
+    pl + geom_violin()
   })
-  
 })
