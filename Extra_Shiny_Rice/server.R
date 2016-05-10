82e13815f616974eb41d1cadb9651f9479ebb4c5
Diane Nguyen <dyen@ucdavis.edu>
2016-05-10 15:24:30 -0700

    Server updates include output functions for violin and boxplot

--- Extra_Shiny_Rice/server.R
+++ Extra_Shiny_Rice/server.R
@@ -11,13 +11,12 @@ library(shiny)
 library(reshape2)
 library(ggplot2)
 
-load("data_from_SNP_lab.Rdata")
+load("/home/bis180l_local/Shiny_Cassandra-Diane-Sarah/data_from_SNP_lab.Rdata")
 data.pheno.mds <- merge(geno.mds, data.pheno, by = "row.names", all = TRUE)
-
 colnames(data.pheno.mds)
 #subset the data to only include the phenotypic traits of interest, popID and Region
-data.pheno.mds1 <- subset(data.pheno.mds[,c(4,11,27,30,31,32,33)])
-head(data.pheno.mds1)
+data.pheno.mds1 <- subset(data.pheno.mds[,c(4,11,27,30,31,32,33,38)])
+head(data.pheno.mds)
 
 #melt the data
 data.pheno.mds.melt <- melt(data.pheno.mds1)
@@ -29,16 +28,34 @@ shinyServer(function(input, output) {
   output$violinPlot <- renderPlot({
     
     # set up the plot
-    pl <- ggplot(data = data.pheno.mds.melt[data.pheno.mds.melt$variable == input$variable,],
+    pl <- ggplot(data = data.pheno.mds.melt[data.pheno.mds.melt$variable == input$trait,],
                  #Use aes_string below so that input$trait is interpreted
                  #correctly.  The other variables need to be quoted
-                 aes_string(x="value",
-                            y=input$trait,
-                            fill="variable"
-                 )
+                 aes_string(x="variable",
+                            y="value",
+                            fill=input$color)
     )
+                 
     
     # draw the violinplot for the specified trait
-    pl + geom_violin()
-  })
-})
+      pl + geom_violin()
+  
+    })
+
+  output$boxplot <- renderPlot({
+    
+    # set up the plot
+    pl <- ggplot(data = data.pheno.mds.melt[data.pheno.mds.melt$variable == input$trait,],
+                 #Use aes_string below so that input$trait is interpreted
+                 #correctly.  The other variables need to be quoted
+                 aes_string(x="variable",
+                            y="value",
+                            fill=input$color)
+    )
+    
+    
+    # draw the scatterplot for the specified trait
+      pl + geom_boxplot()
+    })
+  }
+)
